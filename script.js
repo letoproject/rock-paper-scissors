d167a1f48c7745caae4a689bd89724ae24df8368
letoproject <dmitry.sytnyk@gmail.com>
2023-12-04 18:21:55 +0200

    fix: add game function to play 5 rounds

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const ROCK = "rock";
 const PAPER = "paper";
 const SCISSORS = "scissors";
-
 const CHOICES = [ROCK, PAPER, SCISSORS];
+
+let playerWins = 0;
+let computerWins = 0;
+let gamesTotal = 0;
+let tieCounter = 0;
+let roundWinner = "";
 
 function getComputerChoice() {
   const randomChoice = Math.floor(Math.random() * CHOICES.length);
@@ -30,6 +35,9 @@ function getPlayerSelection() {
   while (!inputValidation(playerSelectionInput)) {
     alert("Input must be a valid string");
     playerSelectionInput = prompt("Input again your choice");
+    if (playerSelectionInput === null) {
+      return null;
+    }
   }
 
   return playerSelectionInput.toLowerCase();
@@ -38,9 +46,11 @@ function getPlayerSelection() {
 function playRound(playerSelection, computerSelection) {
   console.log("playerSelection", playerSelection);
   console.log("computerSelection", computerSelection);
+  roundWinner = "";
 
   if (playerSelection === computerSelection) {
-    return console.log("It's draw. Try one more time");
+    console.log("It's draw.");
+    return (roundWinner = "Tie");
   }
 
   if (
@@ -48,14 +58,47 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === PAPER && computerSelection === SCISSORS) ||
     (playerSelection === SCISSORS && computerSelection === ROCK)
   ) {
-    return console.log(
-      `You Lose! ${computerSelection} beats ${playerSelection}`
-    );
+    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+    return (roundWinner = "computer");
   }
 
-  return console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+  console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+  return (roundWinner = "player");
 }
 
-const playerSelection = getPlayerSelection();
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+  for (let i = 0; i < 5; i += 1) {
+    const playerSelection = getPlayerSelection();
+    const computerSelection = getComputerChoice();
+    gamesTotal += 1;
+
+    playRound(playerSelection, computerSelection);
+    switch (roundWinner) {
+      case "computer":
+        computerWins += 1;
+        break;
+      case "player":
+        playerWins += 1;
+        break;
+      default:
+        tieCounter += 1;
+        break;
+    }
+    console.log("computerWins", computerWins);
+    console.log("playerWins", playerWins);
+    console.log("tieCounter", tieCounter);
+    if (gamesTotal < 5) {
+      console.log("=================== NEXT ROUND ===================");
+    }
+  }
+
+  console.log("=================== ROUND RESULT ===================");
+  if (playerWins > computerWins) {
+    return console.log(`Player win this game with score ${playerWins}`);
+  } else if (playerWins === computerWins) {
+    return console.log("It's tie");
+  }
+  return console.log(`Computer win this game with score ${computerWins}`);
+}
+
+game();
