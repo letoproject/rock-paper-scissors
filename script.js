db1a501bcba64837a277d0732fbfd159a22930e6
letoproject <dmitry.sytnyk@gmail.com>
2023-12-06 19:11:41 +0200

    fix: add UI

--- script.js
+++ script.js
@@ -5,112 +5,71 @@ const CHOICES = [ROCK, PAPER, SCISSORS];
 
 let playerWins = 0;
 let computerWins = 0;
-let gamesTotal = 0;
 let tieCounter = 0;
-let roundWinner = "";
+
+const btns = document.querySelectorAll(".btn-player");
+btns.forEach((btn) => btn.addEventListener("click", getPlayerChoice));
+
+const playerChoiceText = document.querySelector(".selection-text.player");
+const computerChoiceText = document.querySelector(".selection-text.computer");
+const playerScore = document.querySelector(".player-score span");
+const computerScore = document.querySelector(".computer-score span");
+const tieScore = document.querySelector(".tie-score span");
+const roundResult = document.querySelector(".round-winner");
+const gameResult = document.querySelector(".game-winner");
+const vsSpan = document.querySelector(".vs");
+const restartBtn = document.querySelector(".reset");
+
+restartBtn.addEventListener("click", () => location.reload());
 
 function getComputerChoice() {
   const randomChoice = Math.floor(Math.random() * CHOICES.length);
   return CHOICES[randomChoice];
 }
 
-function inputValidation(string) {
-  if (string === "" || string === null) {
-    false;
-  } else if (
-    string.toLowerCase() !== ROCK &&
-    string.toLowerCase() !== PAPER &&
-    string.toLowerCase() !== SCISSORS
-  ) {
-    alert("You need input rock, paper or scissors");
-    false;
-  } else {
-    return string.toLowerCase();
-  }
-}
-
-function getPlayerSelection() {
-  let playerSelectionInput = prompt("Input your choice");
-
-  while (!inputValidation(playerSelectionInput)) {
-    alert("Input must be a valid string");
-    playerSelectionInput = prompt("Input again your choice");
-
-    // for exit from loop (with error)
-    if (playerSelectionInput === null) {
-      break;
-    }
-  }
-
-  return playerSelectionInput.toLowerCase();
+function getPlayerChoice(e) {
+  let playerChoice = CHOICES[e.target.id];
+  playRound(playerChoice, getComputerChoice());
+  vsSpan.classList.remove("hide");
+  vsSpan.classList.add("show");
 }
 
 function playRound(playerSelection, computerSelection) {
-  // for info
-  console.log("playerSelection", playerSelection);
-  console.log("computerSelection", computerSelection);
+  playerChoiceText.textContent = playerSelection;
+  computerChoiceText.textContent = computerSelection;
 
-  roundWinner = "";
+  let roundWinner = "";
 
   if (playerSelection === computerSelection) {
-    console.log("It's draw.");
-    return (roundWinner = "Tie");
-  }
-
-  if (
+    roundResult.textContent = "It's draw.";
+    roundWinner = "Tie";
+    tieScore.textContent = tieCounter += 1;
+  } else if (
     (playerSelection === ROCK && computerSelection === PAPER) ||
     (playerSelection === PAPER && computerSelection === SCISSORS) ||
     (playerSelection === SCISSORS && computerSelection === ROCK)
   ) {
-    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
-    return (roundWinner = "computer");
+    roundResult.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    roundWinner = "computer";
+    computerScore.textContent = computerWins += 1;
+  } else {
+    roundResult.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+    roundWinner = "player";
+    playerScore.textContent = playerWins += 1;
   }
 
-  // if Player win
-  console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
-  return (roundWinner = "player");
+  checkWinner();
 }
 
-function game() {
-  for (let i = 0; i < 5; i += 1) {
-    const playerSelection = getPlayerSelection();
-    const computerSelection = getComputerChoice();
-    gamesTotal += 1;
-
-    playRound(playerSelection, computerSelection);
-    switch (roundWinner) {
-      case "computer":
-        computerWins += 1;
-        break;
-      case "player":
-        playerWins += 1;
-        break;
-      default:
-        tieCounter += 1;
-        break;
-    }
-
-    // for info
-    console.log("computerWins", computerWins);
-    console.log("playerWins", playerWins);
-    console.log("tieCounter", tieCounter);
-
-    // decorator
-    if (gamesTotal < 5) {
-      console.log("=================== NEXT ROUND ===================");
-    }
+function checkWinner() {
+  let message = "";
+  if (computerWins === 5 || playerWins === 5) {
+    message = `${
+      computerWins > playerWins ? "Computer" : "Player"
+    } win this round! 
+     Press Restart button.`;
+    gameResult.textContent = message;
+    btns.forEach((btn) => (btn.disabled = true));
+    btns.forEach((btn) => btn.removeEventListener("click", getPlayerChoice));
   }
-
-  // decorator
-  console.log("=================== ROUND RESULT ===================");
-
-  if (playerWins > computerWins) {
-    return console.log(`Player win this game with score ${playerWins}`);
-  } else if (playerWins === computerWins) {
-    return console.log("It's tie");
-  }
-  // if Computer win
-  return console.log(`Computer win this game with score ${computerWins}`);
 }
-
-game();
